b84284cf1a732086b9da9e94e359888d4cdbf0de
Zziwadavid51 <149446130+Zziwadavid51@users.noreply.github.com>
2023-12-06 15:07:27 +0300

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 21938068
+API_HASH = 'c18fd98f3e58484df0aecd95a3d5a6a9'
+BOT_TOKEN = '6298605796:AAFtqIHBnP6BtBACWGrhEdCRD3czov_QITA'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = True
+
+# Admins, Channels & Users
+ADMINS = [5787509903, 'admin123', 5151096227]
+CHANNELS = [-1001984741311, 'channelusername']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://Ajay:tamil178@tamil.e8uxhjz.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Ajay'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
