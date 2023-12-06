501cc748e35396676e69a78a6836d80721644a99
Zziwadavid51 <149446130+Zziwadavid51@users.noreply.github.com>
2023-12-06 16:29:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Zziwadavid51/my-lady-princess.git /my-lady-princess
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /my-lady-princess
-fi
-cd /my-lady-princess
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+if [ -z $UPSTREAM_REPO ] 
+ then 
+   echo "Cloning main Repository" 
+   git clone https://github.com/TeamHMT/my-lady-princess.git /Auto-search-tamil-bot 
+ else 
+   echo "Cloning Custom Repo from $UPSTREAM_REPO " 
+   git clone $UPSTREAM_REPO /Auto-search-tamil-bot 
+ fi 
+ cd /Auto-search-tamil-bot 
+ pip3 install -U -r requirements.txt 
+ echo "Starting Bot...." 
+ python3 bot.py
